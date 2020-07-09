5a86d2961877d2045ace55759da89d72982f63a0
jk5ho <jk5ho@edu.uwaterloo.ca>
2020-07-09 18:55:14 -0400

    ttest read and write

--- server/app.js
+++ server/app.js
@@ -1,17 +1,31 @@
+const express = require('express');
+const parser = require('body-parser');
+const app = express();
+app.use(parser.json())
 
 const admin = require("firebase-admin");
-
-const serviceAccount = require("/Users/richardma/Desktop/ECE452/server/ece452-297ff-firebase-adminsdk-o4qg7-41afcae2be.json");
+const key = require("./ece452-297ff-firebase-adminsdk-o4qg7-41afcae2be.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(key),
   databaseURL: "https://ece452-297ff.firebaseio.com"
 });
 
 const db = admin.database();
-const ref = db.ref("test");
-ref.once("value", function(snapshot) {
-  console.log(snapshot.val());
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
+const ref = db.ref("Receipt");
+
+app.get("/read", (req, res) => {
+  ref.once("value", function(snapshot) {
+    res.send(snapshot.val(), 200)
+  }, function (errorObject) {
+    res.send("The read failed: " + errorObject.code, 400);
+  });
 });
+
+app.post("/write", (req, res) => {
+  console.log(req.body)
+  ref.child("receipt").set(req.body);
+  res.send("Gucci", 200)
+})
+
+module.exports = app;
