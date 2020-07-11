528fe2d280009ea2e449d1c69debda7bc4de8dd0
jk5ho <jk5ho@edu.uwaterloo.ca>
2020-07-11 18:45:16 -0400

    server-side completed

--- server/app.js
+++ server/app.js
@@ -15,8 +15,14 @@ const db = admin.database();
 const ref = db.ref("Receipt");
 
 app.get("/read", (req, res) => {
-  let payer = req.query.payer
-  let payee = req.query.payee
+  if(req.query.payer.localeCompare(req.query.payee)) {
+    let payer = req.query.payer
+    let payee = req.query.payee
+  } else {
+    let payee = req.query.payer
+    let payer = req.query.payee
+  }
+
   ref.once("value", function(snapshot) {
     let ret = snapshot.val()
     console.log(ret)
@@ -35,9 +41,48 @@ app.get("/read", (req, res) => {
 });
 
 app.post("/write", (req, res) => {
-  console.log(req.body)
-  ref.child("receipt").set(req.body);
-  res.send("Gucci", 200)
+  let id_A = req.body.userA;
+  let id_B = req.body.userB;
+  let blnc = req.body.balance;
+  
+  // sort lexicographically
+  let entry = {}
+  if(id_A.localeCompare(id_B) < 0) {
+    entry = {
+      payer : id_A,
+      payee : id_B,
+      balance : blnc  
+    }
+  } else {
+    entry = {
+      payer : id_B,
+      payee : id_A,
+      balance : blnc  
+    }
+  }
+
+  // search if already exists
+  let keyPrev = "";
+  ref.once("value", function(snapshot) {
+    for(let temp in snapshot.val()) {
+      let curr = snapshot.val()[temp];
+      if(curr["payer"] === id_A && curr["payee"] === id_B) {
+        keyPrev = temp;
+      } else if(curr["payer"] === id_B && curr["payee"] === id_A) {
+        keyPrev = temp;
+      }
+    }
+  }, function (errorObject) {
+    res.send("The read failed: " + errorObject.code, 400);
+  });
+
+  // replace or add new entry
+  if(keyPrev != "") {
+    ref.child(keyPrev).set(entry);
+  } else {
+    ref.push(entry);
+  }
+  res.send("Balance Updated for ", id_A, " and ", id_B, 200)
 })
 
 module.exports = app;
