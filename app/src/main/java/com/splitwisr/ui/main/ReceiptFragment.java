de3d407cd62af0b12906c5ef9178ae25ef991163
Aidan Wood <aidanrbwood@gmail.com>
2020-07-11 15:55:38 -0400

    new branch with receipt stuff

---
+++ app/src/main/java/com/splitwisr/ui/main/ReceiptFragment.java
@@ -0,0 +1,81 @@
+package com.splitwisr.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.splitwisr.R;
+import com.splitwisr.databinding.ReceiptFragmentBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReceiptFragment extends Fragment {
+
+    private EditText receiptAmount;
+    private EditText userList;
+    private Button submitReceipt;
+
+    private ReceiptFragmentBinding binding;
+
+    private double currentReceiptAmount = 0d;
+    private List<String> currentUsers = new ArrayList<String>();
+
+    public static ReceiptFragment newInstance() {return new ReceiptFragment();}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = ReceiptFragmentBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        receiptAmount = (EditText)view.findViewById(R.id.receiptAmount);
+        userList = (EditText)view.findViewById(R.id.userList);
+        submitReceipt = (Button)view.findViewById(R.id.receiptButton);
+
+        receiptAmount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentReceiptAmount = Double.parseDouble(receiptAmount.getText().toString());
+            }
+        });
+
+        userList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] s = userList.getText().toString().split(System.getProperty("line.separator"));
+                currentUsers.clear();
+                for (String user : s) {
+                    currentUsers.add(user);
+                }
+            }
+        });
+
+        submitReceipt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentReceiptAmount > 0d) {
+                    int numUsers = currentUsers.size() + 1;
+                    double amountPerUser = currentReceiptAmount/numUsers;
+
+                }
+            }
+        });
+
+
+
+        // Setup recyclerView
+        binding.recyclerView.setHasFixedSize(true);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+
+        return view;
+    }
+}
