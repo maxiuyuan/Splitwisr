a69ba6b635b465429106c788a15f4f00d4faf141
Zahin Mohammad <zahin.dev@gmail.com>
2020-07-12 02:13:41 -0400

    Firebase Auth (#2)
    * update gitignore
    
    * update dependencies for firebase
    
    * add google sign in
    
    * wip, google sign in broken
    
    * done auth?

--- app/src/main/java/com/splitwisr/MainActivity.java
+++ app/src/main/java/com/splitwisr/MainActivity.java
@@ -1,15 +1,26 @@
 package com.splitwisr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseAuth mAuth;
+    NavController navController;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +28,66 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main_activity);
         setSupportActionBar(findViewById(R.id.toolbar));
 
-        NavController navController =
+        this.initNav();
+        this.initFirebase();
+
+    }
+
+
+
+    private void initNav() {
+        navController =
                 Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(
                 (BottomNavigationView) findViewById(R.id.bottom_navigation), navController);
     }
 
+    private void initFirebase() {
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(this.toString(), "onAuthStateChanged:signed_in:" + user.getUid());
+                    findViewById(R.id.bottom_navigation).setVisibility(View.VISIBLE);
+                } else {
+                    // User is signed out
+                    Log.d(this.toString(), "onAuthStateChanged:signed_out");
+                    findViewById(R.id.bottom_navigation).setVisibility(View.GONE);
+                }
+            }
+        };
+    }
+    // Add Auth state listener in onStart method.
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.log_out && mAuth.getCurrentUser()!= null) {
+           mAuth.signOut();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
