e644e17bf7b6bd8827bc68726d9343005ef16f3f
Zahin Mohammad <zahin.mohammad@gmail.com>
2020-07-12 21:23:52 -0400

    use getView()

--- app/src/main/java/com/splitwisr/ui/login/LoginFragment.java
+++ app/src/main/java/com/splitwisr/ui/login/LoginFragment.java
@@ -69,16 +69,16 @@ public class LoginFragment extends Fragment {
 
         super.onViewCreated(view, savedInstanceState);
         Log.d(this.toString(), "bhaiiii");
-        requireActivity().findViewById(R.id.google_sign_in_button).setOnClickListener(v -> {
+        getView().findViewById(R.id.google_sign_in_button).setOnClickListener(v -> {
             Log.d(this.toString(), "bhaiiii");
             Intent signInIntent = mGoogleSignInClient.getSignInIntent();
             startActivityForResult(signInIntent, RC_SIGN_IN);
         });
-        requireActivity().findViewById(R.id.sign_out_button).setOnClickListener(v -> {
+        getView().findViewById(R.id.sign_out_button).setOnClickListener(v -> {
             mGoogleSignInClient.signOut();
             mAuth.signOut();
         });
-        requireActivity().findViewById(R.id.skipButton).setOnClickListener(v -> {
+        getView().findViewById(R.id.skipButton).setOnClickListener(v -> {
             NavHostFragment
                     .findNavController(this)
                     .navigate(R.id.action_destination_login_fragment_to_destination_balance_fragment);
