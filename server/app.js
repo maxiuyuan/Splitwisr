f4c22350a571983f34a48182abbe18e35d5d202f
Joey Ho <jk5ho@uwaterloo.ca>
2020-07-22 17:35:21 -0400

    GCM Push Notification  (#5)
    * added push notification endpoints
    
    * added gcm logic
    
    * email mailer for non-registered users
    
    Co-authored-by: jk5ho <jk5ho@edu.uwaterloo.ca>

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const parser = require('body-parser');
-const app = express();
-app.use(parser.json())
-
 const admin = require("firebase-admin");
+const gcm = require("node-gcm");
+const mailer = require("nodemailer");
 const key = require("./ece452-297ff-firebase-adminsdk-o4qg7-41afcae2be.json");
+
+const server = express();
+server.use(parser.json());
 
 admin.initializeApp({
   credential: admin.credential.cert(key),
@@ -12,54 +14,172 @@ admin.initializeApp({
 });
 
 const db = admin.database();
-const ref = db.ref("Receipt");
+const baseRef = db.ref();
+const tokenRef = db.ref("Token");
+const receiptRef = db.ref("Receipt");
 
-app.get("/read", (req, res) => {
+// ########## Notification ##########
+
+// endpoint for device registration
+server.post("/register", (req, res) => {
+  let current_user = req.query.current_user;
+  let device_id = req.query.device_id;
+  
+  tokenRef.once("value", function(snapshot) {
+    let keyPrev = "";
+    let entry = {
+      user : current_user,
+      device : device_id
+    }
+
+    // search if already exists
+    for(let temp in snapshot.val()) {
+      let curr = snapshot.val()[temp];
+      if(curr["user"] === current_user) {
+        keyPrev = temp;
+        break;
+      };
+    }
+
+    // replace or add new entry
+    if(keyPrev != "") {
+      tokenRef.child(keyPrev).set(entry);
+    } else {
+      tokenRef.push(entry);
+    }
+    res.status(200).send("Device registered for " + current_user)
+  }, function (errorObject) {
+    res.status(400).send("The register failed: " + errorObject.code);
+  });
+});
+
+// endpoint for sending to GCM
+server.get("/send", (req, res) => {
+  let current_user = req.query.current_user;
+  let target_user = req.query.target_user;
+
+  baseRef.once("value", function(snapshot) {
+    let owing = 0;
+    let target_device = "";
+
+    // aggregate balances between two users
+    let receipt = snapshot.val().Receipt;
+    for(let temp in receipt) {
+      let curr = receipt[temp];
+      if(curr["payer"] === current_user && curr["payee"] === target_user) {
+        owing -= parseFloat(curr["balance"]);
+      } else if(curr["payer"] === target_user && curr["payee"] === current_user) {
+        owing += parseFloat(curr["balance"]);
+      }
+    }
+
+    // retreive target device
+    let token = snapshot.val().Token;
+    for(let temp in token) {
+      let curr = token[temp];
+      if(curr["user"] === target_user) {
+        target_device = curr["device"];
+      }
+    }
+
+    if(target_device != "") {
+      // send GCM notify when device is registered
+      sendAndroid(current_user, target_device, owing);
+    } else {
+      // send email when device is not registered
+      sendEMail(current_user, target_user, owing);
+    }
+
+    res.status(200).send("Owing notified " + owing + " to " + target_user);
+  }, function (errorObject) {
+    res.status(400).send("The notify failed: " + errorObject.code);
+  });
+});
+
+// helper function to deliver GCM notification (registered users)
+function sendAndroid(current_user, target_device, owing) {
+  let message = new gcm.Message({
+    notification : {
+        title : current_user + " reminds you that you owe " + owing
+    }
+  });
+
+  let sender = new gcm.sender(key);
+  sender.send(message, target_device);
+}
+
+// helper function to deliver email notification (non-registered users)
+function sendEMail(current_user, target_user, owing) {
+  
+  let transporter = mailer.createTransport({
+    service: "service",
+    port: 1234,
+    auth: {
+      user: "abcde",
+      pass: "abcde"
+    }
+  });
+  
+  let mailOptions = {
+    from: '"Splitwisr Team <noreply@splitwisr.com>"',
+    to: target_user,
+    subject: 'Splitwisr Balance Reminder',
+    text: 'Your friend ' + current_user + ' reminds you that you owe ' + owing,
+    html: '<b> Hi there! </b> <br> This is an automated mail from Splitwisr!'
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if(error) {
+      return "Message delivery has failed " + error;
+    } else {
+      return "Message delivered: " + info.message;
+    }
+  });
+}
+
+// ########## Balances ##########
+
+// endpoint for reading balances
+server.get("/read", (req, res) => {
   let current_user = req.query.current_user;
   let userToBalance = []
-  
-  ref.once("value", function(snapshot) {
+
+  receiptRef.once("value", function(snapshot) {
     let ret = snapshot.val()
     for (let transaction in ret){
       if(ret[transaction]['payer'] === current_user || ret[transaction]['payee'] === current_user){
         userToBalance.push(ret[transaction])
       }
     }
-    res.send(userToBalance, 200)
+    res.status(200).send(userToBalance)
   }, function (errorObject) {
-    res.send("The read failed: " + errorObject.code, 400);
+    res.status(400).send("The read failed: " + errorObject.code);
   });
 });
 
-
-app.post("/write", (req, res) => {
+// endpoint for writing balances
+server.post("/write", (req, res) => {
   let id_A = req.query.payer;
   let id_B = req.query.payee;
   let blnc = req.query.balance;
 
-  // sort lexicographically
-  let entry = {}
+  // check lexicographic order
   if(!(id_A.localeCompare(id_B) < 0)) {
-    res.send("Not lexicographically sorted!", 400);
-
+    res.status(400).send("Not lexicographically sorted!");
   }
 
-  entry = {
-    payer : id_A,
-    payee : id_B,
-    balance : blnc
-  }
-
-  // search if already exists
-  ref.once("value", function(snapshot) {
+  receiptRef.once("value", function(snapshot) {
     let keyPrev = "";
+    let entry = {
+      payer : id_A,
+      payee : id_B,
+      balance : blnc
+    }
 
+    // search if already exists
     for(let temp in snapshot.val()) {
       let curr = snapshot.val()[temp];
       if(curr["payer"] === id_A && curr["payee"] === id_B) {
-        keyPrev = temp;
-        break;
-      } else if(curr["payer"] === id_B && curr["payee"] === id_A) {
         keyPrev = temp;
         break;
       }
@@ -67,14 +187,14 @@ app.post("/write", (req, res) => {
 
     // replace or add new entry
     if(keyPrev != "") {
-      ref.child(keyPrev).set(entry);
+      receiptRef.child(keyPrev).set(entry);
     } else {
-      ref.push(entry);
+      receiptRef.push(entry);
     }
-    res.send("Balance Updated for ", id_A, " and ", id_B, 200)
+    res.status(200).send("Balance Updated for " + id_A + " and " + id_B);
   }, function (errorObject) {
-    res.send("The read failed: " + errorObject.code, 400);
+    res.status(400).send("The write failed: " + errorObject.code);
   });
-})
+});
 
-module.exports = app;
+module.exports = server;
