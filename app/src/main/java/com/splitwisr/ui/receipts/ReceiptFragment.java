72c8c3a980161409ecbf8a42c9fea0bb6db17b98
Aidan Wood <aidanrbwood@gmail.com>
2020-07-29 03:08:02 -0400

    my laptop froze and now im paranoid

--- app/src/main/java/com/splitwisr/ui/receipts/ReceiptFragment.java
+++ app/src/main/java/com/splitwisr/ui/receipts/ReceiptFragment.java
@@ -1,11 +1,16 @@
 package com.splitwisr.ui.receipts;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,6 +31,17 @@ import java.util.HashMap;
 import java.util.List;
 
 public class ReceiptFragment extends Fragment {
+    class ReceiptItem {
+        public String name;
+        public Double cost;
+        public List<String> usersSplitting;
+
+        ReceiptItem(String s, Double d) {
+            name = s;
+            cost = d;
+            usersSplitting = new ArrayList<>();
+        }
+    }
     private ReceiptsViewModel receiptsViewModel;
     private ReceiptFragmentBinding binding;
 
@@ -38,6 +54,20 @@ public class ReceiptFragment extends Fragment {
     private HashMap<String, Double> amountsOwed = new HashMap<>();
     private String selectedUser = "";
 
+    private List<TextView> usersSplittingItemViews = new ArrayList<>();
+
+    private HashMap<Integer, ReceiptItem> receiptItems = new HashMap<>();
+
+    private String[] selectableUsers;
+
+    private final String itemTextBaseId = "ItemText";
+    private final String itemButtonBaseId = "ItemButton";
+
+    private final LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+    private int itemId = 0;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -48,45 +78,11 @@ public class ReceiptFragment extends Fragment {
         receiptsViewModel = new ViewModelProvider(requireActivity()).get(ReceiptsViewModel.class);
         List<User> users = receiptsViewModel.getUserList();
 
-        if (users != null) {
-            for (int x = 0; x < users.size(); x++) {
-                String userName = users.get(x).firstName + " " + users.get(x).lastName;
-                userNames.put(userName, users.get(x).email);
-            }
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                    requireContext(),
-                    android.R.layout.simple_spinner_dropdown_item,
-                    userNames.keySet().toArray(new String[0])
-            );
-            binding.userDropDown.setAdapter(adapter);
-        } else {
-            String[] s = new String[]{"no users"};
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                    requireContext(),
-                    android.R.layout.simple_spinner_dropdown_item,
-                    s
-            );
-            binding.userDropDown.setAdapter(adapter);
+        for (int x = 0; x < users.size(); x++) {
+            String userName = users.get(x).firstName + " " + users.get(x).lastName;
+            userNames.put(userName, users.get(x).email);
         }
-
-        binding.userDropDown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-                selectedUser = (String) parent.getItemAtPosition(pos);
-            }
-
-
-            public void onNothingSelected(AdapterView<?> parent) {
-                selectedUser = "";
-            }
-        });
-
-        binding.addUserToItemButton.setOnClickListener(v -> {
-            if (!selectedUser.equals("") && !usersToSplitItem.contains(selectedUser)) {
-                usersToSplitItem.add(userNames.get(selectedUser));
-                userNamesToSplitItem.add(selectedUser);
-                addUsersSplittingItemText(selectedUser);
-            }
-        });
+        selectableUsers = userNames.keySet().toArray(new String[0]);
 
         binding.addItemButton.setOnClickListener(v -> {
             double tempItemCost = 0d;
@@ -97,29 +93,57 @@ public class ReceiptFragment extends Fragment {
             }
             tempItemName = binding.itemNameText.getText().toString();
 
-            if (tempItemCost > 0d && usersToSplitItem.size() > 0) {
-                double costPerUser = round(tempItemCost/usersToSplitItem.size());
-                StringBuilder s = new StringBuilder("\n" + tempItemName + " - " + tempItemCost + " ");
-                for (int x = 0; x < userNamesToSplitItem.size(); x++) {
-                    String user = usersToSplitItem.get(x);
-                    String userName = userNamesToSplitItem.get(x);
-                    if (!user.equals(receiptsViewModel.getCurrentUserEmail())) {
-                        if (!amountsOwed.containsKey(user)) {
-                            amountsOwed.put(user, 0d);
-                        }
-                        amountsOwed.put(user, amountsOwed.get(user) + costPerUser);
+            if (tempItemCost > 0d) {
+                receiptItems.put(itemId, new ReceiptItem(tempItemName, tempItemCost));
+//                double costPerUser = round(tempItemCost/usersToSplitItem.size());
+//                StringBuilder s = new StringBuilder("\n" + tempItemName + " - " + tempItemCost + " ");
+//                for (int x = 0; x < userNamesToSplitItem.size(); x++) {
+//                    String user = usersToSplitItem.get(x);
+//                    String userName = userNamesToSplitItem.get(x);
+//                    if (!user.equals(receiptsViewModel.getCurrentUserEmail())) {
+//                        if (!amountsOwed.containsKey(user)) {
+//                            amountsOwed.put(user, 0d);
+//                        }
+//                        amountsOwed.put(user, amountsOwed.get(user) + costPerUser);
+//                    }
+//                    s.append(userName);
+//                    if (x != userNamesToSplitItem.size() - 1) s.append(", ");
+//                }
+//                addReceiptContentsText(s.toString());
+//                usersToSplitItem.clear();
+//                userNamesToSplitItem.clear();
+//                resetUsersSplittingItemText();
+                LinearLayout ll = new LinearLayout(this.getContext());
+                ll.setOrientation(LinearLayout.HORIZONTAL);
+                TextView itemView = new TextView(this.getContext());
+                itemView.setText(tempItemName + " " + tempItemCost);
+                ll.addView(itemView);
+                Button addUsers = new Button(this.getContext());
+                addUsers.setId(itemId);
+                addUsers.setLayoutParams(params);
+                addUsers.setText("add users");
+                addUsers.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        selectUsers(v.getId());
                     }
-                    s.append(userName);
-                    if (x != userNamesToSplitItem.size() - 1) s.append(", ");
-                }
-                addReceiptContentsText(s.toString());
-                usersToSplitItem.clear();
-                userNamesToSplitItem.clear();
-                resetUsersSplittingItemText();
+                });
+                ll.addView(addUsers);
+                TextView userView = new TextView(this.getContext());
+                userView.setText("");
+                usersSplittingItemViews.add(userView);
+                ll.addView(userView);
+                binding.receiptLinearLayout.addView(ll);
+                itemId++;
             }
         });
 
         binding.submitButton.setOnClickListener(v -> {
+            for (ReceiptItem item : receiptItems.values()) {
+                
+                double costPerUser = round(item.cost/item.usersSplitting.size());
+
+            }
             for (String splitUser : amountsOwed.keySet()) {
                 double amountOwed = amountsOwed.get(splitUser);
                 if (!splitUser.equals(receiptsViewModel.getCurrentUserEmail())) {
@@ -141,16 +165,58 @@ public class ReceiptFragment extends Fragment {
             }
             if (!amountsOwed.isEmpty()){
                 amountsOwed.clear();
-                resetReceiptContentsText();
+                //resetReceiptContentsText();
                 NavController navController = NavHostFragment.findNavController(this);
                 navController.navigate(R.id.destination_balance_fragment);
             }
         });
-
-        resetReceiptContentsText();
-        resetUsersSplittingItemText();
-
         return view;
+    }
+
+    void selectUsers (int id) {
+        boolean[] selectedUsers = new boolean[selectableUsers.length];
+        AlertDialog.Builder builder = new AlertDialog.Builder(this.getContext());
+        builder.setTitle("users splitting").setMultiChoiceItems(selectableUsers, selectedUsers, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                selectedUsers[which] = isChecked;
+            }
+        });
+        builder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                addUsersToItem(id, selectedUsers);
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    void addUsersToItem(int id, boolean[] selectedUsers) {
+        if (receiptItems.containsKey(id)) {
+            ReceiptItem item = receiptItems.get(id);
+            StringBuilder names = new StringBuilder();
+            item.usersSplitting.clear();
+            boolean first = true;
+            for (int x = 0; x < selectedUsers.length; x++) {
+                if (selectedUsers[x]) {
+                    item.usersSplitting.add(userNames.get(selectableUsers[x]));
+                    if (first) {
+                        first = false;
+                    } else {
+                        names.append(", ");
+                    }
+                    names.append(selectableUsers[x].split(" ")[0]);
+                }
+            }
+            usersSplittingItemViews.get(id).setText(names.toString());
+        }
     }
 
     // Rounding function for rounding to 2 decimal places
@@ -160,23 +226,4 @@ public class ReceiptFragment extends Fragment {
         return bd.doubleValue();
     }
 
-    public void resetUsersSplittingItemText() {
-        usersSplittingItemText = new StringBuilder("Users to Split Item:");
-        binding.usersSplittingItem.setText(usersSplittingItemText.toString());
-    }
-
-    public void resetReceiptContentsText() {
-        receiptContentsText = new StringBuilder("Receipt Contents:");
-        binding.receiptContents.setText(receiptContentsText.toString());
-    }
-
-    public void addUsersSplittingItemText(String user) {
-        usersSplittingItemText.append("\n").append(user);
-        binding.usersSplittingItem.setText(usersSplittingItemText.toString());
-    }
-
-    public void addReceiptContentsText(String s) {
-        receiptContentsText.append(s);
-        binding.receiptContents.setText(receiptContentsText.toString());
-    }
 }
