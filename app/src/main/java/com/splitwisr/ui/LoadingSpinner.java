137ca61db57a14921468a90988e33d4b8d09e5f5
Zahin Mohammad <zahin.mohammad@gmail.com>
2020-07-29 20:46:35 -0400

    clean up

--- app/src/main/java/com/splitwisr/ui/LoadingSpinner.java
+++ app/src/main/java/com/splitwisr/ui/LoadingSpinner.java
@@ -1,23 +1,14 @@
 package com.splitwisr.ui;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.splitwisr.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LoadingSpinner#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LoadingSpinner extends Fragment {
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +17,6 @@ public class LoadingSpinner extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_loading_spinner, container, false);
     }
 }
