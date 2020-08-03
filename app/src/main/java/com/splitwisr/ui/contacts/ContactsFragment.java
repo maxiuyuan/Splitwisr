00843285f5d556781979df87da4c8ecff19797dd
Brian Norman <briankn8@gmail.com>
2020-08-03 08:27:35 -0600

    Add MultiSelect for group members

--- app/src/main/java/com/splitwisr/ui/contacts/ContactsFragment.java
+++ app/src/main/java/com/splitwisr/ui/contacts/ContactsFragment.java
@@ -2,6 +2,7 @@ package com.splitwisr.ui.contacts;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,9 +18,12 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.abdeveloper.library.MultiSelectDialog;
 import com.splitwisr.R;
 import com.splitwisr.data.users.User;
 import com.splitwisr.databinding.ContactsFragmentBinding;
+
+import java.util.ArrayList;
 
 public class ContactsFragment extends Fragment {
     private ContactsFragmentBinding binding;
@@ -37,7 +41,7 @@ public class ContactsFragment extends Fragment {
         binding.userRecycler.setAdapter(usersAdapter);
         binding.userRecycler.addItemDecoration(new DividerItemDecoration(requireContext(), LinearLayout.VERTICAL));
 
-        binding.submit.setOnClickListener(v -> {
+        binding.addUserButton.setOnClickListener(v -> {
             String newEmail = binding.email.getText().toString();
             String newFirstName = binding.firstName.getText().toString();
             String newLastName = binding.lastName.getText().toString();
@@ -45,13 +49,41 @@ public class ContactsFragment extends Fragment {
             if (!newEmail.isEmpty() && !newFirstName.isEmpty() && !newLastName.isEmpty()) {
                 User newUser = new User(newEmail, newFirstName, newLastName);
                 viewModel.insertUser(newUser);
-                InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                hideKeyboard(view);
                 binding.email.setText("");
                 binding.firstName.setText("");
                 binding.lastName.setText("");
             }
         });
+
+        binding.addGroupButton.setOnClickListener(v -> {
+            String newGroupName = binding.groupName.getText().toString();
+
+            if (!newGroupName.isEmpty()) {
+                MultiSelectDialog multiSelectDialog = new MultiSelectDialog()
+                        .title("Add Members to " + newGroupName)
+                        .titleSize(18)
+                        .positiveText("Done")
+                        .negativeText("Cancel")
+                        .setMinSelectionLimit(2)
+                        .multiSelectList(viewModel.getUserMultiSelectList())
+                        .onSubmit(new MultiSelectDialog.SubmitCallbackListener() {
+                            @Override
+                            public void onSelected(ArrayList<Integer> selectedIds, ArrayList<String> selectedNames, String dataString) {
+                                // TODO: Add group
+                            }
+
+                            @Override
+                            public void onCancel() {
+                                Log.d("ContactsFragment","Dialog cancelled");
+                            }
+                        });
+                multiSelectDialog.show(getParentFragmentManager(), "multiSelectDialog");
+                hideKeyboard(view);
+                binding.groupName.setText("");
+            }
+        });
+
         binding.importContactsFab.setOnClickListener(v->{
             NavController navController = NavHostFragment.findNavController(this);
             navController.navigate(R.id.destination_import_contacts);
@@ -73,6 +105,11 @@ public class ContactsFragment extends Fragment {
             binding.emptyStateImage.setVisibility(
                     (users == null || users.size() == 1) ? View.VISIBLE : View.GONE);
         });
+    }
+
+    private void hideKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
     @Override
--- app/src/main/java/com/splitwisr/ui/contacts/ContactsViewModel.java
+++ app/src/main/java/com/splitwisr/ui/contacts/ContactsViewModel.java
@@ -6,10 +6,14 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
+import com.abdeveloper.library.MultiSelectModel;
 import com.splitwisr.data.users.User;
 import com.splitwisr.data.users.UserRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ContactsViewModel extends AndroidViewModel {
     private UserRepository userRepository;
@@ -25,5 +29,13 @@ public class ContactsViewModel extends AndroidViewModel {
 
     public void insertUser(User user) {
         userRepository.upsert(user);
+    }
+
+    public ArrayList<MultiSelectModel> getUserMultiSelectList() {
+        List<User> users = userRepository.getUserList();
+        return IntStream
+                .range(0, users.size())
+                .mapToObj(i -> new MultiSelectModel(i, users.get(i).firstName + " " + users.get(i).lastName))
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 }
